60c92ccbc7d4437b514906d669cab0b541aa542e
JohnThunHC007 <43962854+JohnThunHC007@users.noreply.github.com>
2021-10-14 14:03:47 +0800

    Update Spring5webappApplication.java
    test

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,7 +2,9 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+* Test
+*/
 @SpringBootApplication
 public class Spring5webappApplication {
 
